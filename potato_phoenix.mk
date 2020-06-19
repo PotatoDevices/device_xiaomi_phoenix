df34edcc9f44d877f8204ca5bcf8111678ec6fcf
Pranav Vashi <neobuddy89@gmail.com>
2020-06-19 09:13:43 +0530

    phoenix: Update build fingerprint Coral June

--- potato_phoenix.mk
+++ potato_phoenix.mk
@@ -18,6 +18,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys
 
 PRODUCT_ARCH := arm64
